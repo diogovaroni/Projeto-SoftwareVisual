60f83eab0ee1e9dc0139089b17d1bbf7c747d14e
Varoni <varonidiogo@gmail.com>
2022-09-10 18:23:32 -0300

    Criação do CRUD da página 'Departamentos' através do Scaffolding

---
+++ RegistroVendas/Data/RegistroVendasContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RegistroVendas.Models;
+
+namespace RegistroVendas.Data
+{
+    public class RegistroVendasContext : DbContext
+    {
+        public RegistroVendasContext (DbContextOptions<RegistroVendasContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RegistroVendas.Models.Departamento> Departamento { get; set; }
+    }
+}
--- RegistroVendas/Startup.cs
+++ RegistroVendas/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RegistroVendas.Data;
 
 namespace RegistroVendas
 {
@@ -33,6 +35,9 @@ namespace RegistroVendas
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<RegistroVendasContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("RegistroVendasContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
