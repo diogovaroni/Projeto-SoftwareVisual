9d30f888537aa04036273807531e3bc12c4121f5
Varoni <varonidiogo@gmail.com>
2022-09-10 17:24:06 -0300

    Criação da classe 'Departamento' e da View e Controller 'Departamentos'

---
+++ RegistroVendas/Controllers/DepartamentosController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RegistroVendas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistroVendas.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departamento> list = new List<Departamento>();
+            list.Add(new Departamento { Id = 1, Nome = "Vestuário" });
+            list.Add(new Departamento { Id = 2, Nome = "Decoração" });
+            return View(list);
+        }
+    }
+}
