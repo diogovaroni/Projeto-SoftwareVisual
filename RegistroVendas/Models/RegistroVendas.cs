da6106a9fcd68f92eca983f4425bde297b0d3367
Varoni <varonidiogo@gmail.com>
2022-09-15 23:49:43 -0300

    Criação das classes 'RegistroVendas' e 'VendasStatus'

---
+++ RegistroVendas/Models/RegistroVendas.cs
@@ -0,0 +1,14 @@
+﻿using RegistroVendas.Models.Enums;
+using System;
+
+
+namespace RegistroVendas.Models
+{
+    public class RegistroVendas
+    {
+        public int Id { get; set; }
+        public DateTime DataVenda { get; set; }
+        public double Valor { get; set; }
+        public VendaStatus Status { get; set; }
+    }
+}
