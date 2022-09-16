e542b5706ec55f6cad302e6ade139390aac6c798
Varoni <varonidiogo@gmail.com>
2022-09-15 23:04:00 -0300

    Criação da classe 'Vendedor'

---
+++ RegistroVendas/Models/Vendedor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistroVendas.Models
+{
+    public class Vendedor
+    {
+        public int Id { get; set; }
+        public String Nome { get; set; }
+        public String Email { get; set; }
+        public String CPF { get; set; }
+        public double BaseSalarial { get; set; }
+    }
+}
+
+
+
+
