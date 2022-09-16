eecd114feb88a1ed1c179fe93a21fe4ed92b4433
Varoni <varonidiogo@gmail.com>
2022-09-15 23:56:14 -0300

    Update classe 'VendaStatus'

--- RegistroVendas/Models/Enums/VendaStatus.cs
+++ RegistroVendas/Models/Enums/VendaStatus.cs
@@ -5,7 +5,12 @@ using System.Threading.Tasks;
 
 namespace RegistroVendas.Models.Enums
 {
-    public class VendaStatus
+    public enum VendaStatus : int
     {
+        Pendente = 0,
+        Faturado = 1,
+        Cancelado = 2
     }
 }
+
+
